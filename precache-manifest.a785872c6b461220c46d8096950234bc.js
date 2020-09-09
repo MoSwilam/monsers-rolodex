15194d2bc35546950cefbfa996d08252477ecd34
mo.sw <mo.sw@ideasoft.io>
2020-09-09 12:26:19 +0300

    Updates

---
+++ precache-manifest.a785872c6b461220c46d8096950234bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c860e74f1d34f89624db825ea63a2c36",
+    "url": "/monsers-rolodex/index.html"
+  },
+  {
+    "revision": "336737ccfe8f199299b6",
+    "url": "/monsers-rolodex/static/css/main.f8ec4d19.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsers-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsers-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "336737ccfe8f199299b6",
+    "url": "/monsers-rolodex/static/js/main.5b29241e.chunk.js"
+  },
+  {
+    "revision": "fca51003385a44755cbc",
+    "url": "/monsers-rolodex/static/js/runtime-main.015b96a7.js"
+  }
+]);
